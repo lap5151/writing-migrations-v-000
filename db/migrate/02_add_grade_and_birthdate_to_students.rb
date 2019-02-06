24b9c025494bcab82ae958b17b557b184531655d
Lauren Pardee <lap5151@gmail.com>
2019-02-06 23:52:45 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -3,7 +3,6 @@ class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
     add_column :artists, :birthdate, :string
     add_column :artists, :grade, :integer
-    end
   end
 
 end
